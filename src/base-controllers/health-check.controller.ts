f554b47f150c3cffcb0a62c4c8e12ab51778356d
Oksana <demskaya01@gmail.com>
2023-03-16 14:25:48 +0300

    deployment check

--- src/base-controllers/health-check.controller.ts
+++ src/base-controllers/health-check.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
 export const healthCheckController = (req: Request, res: Response) => {
-  res.status(200).send('Ok');
+  res.status(200).send('Super');
 };
